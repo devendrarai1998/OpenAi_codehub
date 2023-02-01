4803d58c22e68f0c0757963116f59a27a8cbeac5
Devendra Kumar Rai <68963294+devendrarai1998@users.noreply.github.com>
2023-02-01 23:30:56 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -1,28 +1,13 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
-const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
 
-let loadInterval
-
-
-// function loader(element) {
-//   element.textContent = ''
-
-//   loadInterval = setInterval(() => {
-//     // Update the text content of the loading indicator
-//     element.textContent += '.';
-
-//     // If the loading indicator has reached three dots, reset it
-//     if (element.textContent === '....') {
-//       element.textContent = '';
-//     }
-//   }, 300);
-// }
+let loadInterval;
 
 function loader(element) {
-  element.textContent = ''
+  element.textContent = '';
 
   loadInterval = setInterval(() => {
     // Update the text content of the loading indicator
@@ -33,34 +18,20 @@ function loader(element) {
       element.textContent = '';
     }
   }, 300);
-}
-
-
-// function typeText(element, text) {
-//   let index = 0
-
-//   let interval = setInterval(() => {
-//     if (index < text.length) {
-//       element.innerHTML += text.charAt(index)
-//       index++
-//     } else {
-//       clearInterval(interval)
-//     }
-//   }, 20)
-// }
+};
 
 function typeText(element, text) {
-  let index = 0
+  let index = 0;
 
   let interval = setInterval(() => {
     if (index < text.length) {
-      element.innerHTML += text.charAt(index)
-      index++
+      element.innerHTML += text.charAt(index);
+      index++;
     } else {
-      clearInterval(interval)
+      clearInterval(interval);
     }
-  }, 20)
-}
+  }, 20);
+};
 
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
@@ -71,16 +42,16 @@ function generateUniqueId() {
   const hexadecimalString = randomNumber.toString(16);
 
   return `id-${timestamp}-${hexadecimalString}`;
-}
+};
 
-function chatStripe(isAi, value, uniqueId) {
+function chatStripe (isAi, value, uniqueId) {
   return (
     `
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
                 <div class="profile">
                     <img 
-                      src=${isAi ? bot : user} 
+                      src="${isAi ? bot : user}" 
                       alt="${isAi ? 'bot' : 'user'}" 
                     />
                 </div>
@@ -89,93 +60,54 @@ function chatStripe(isAi, value, uniqueId) {
         </div>
     `
   )
-}
-
-// const handleSubmit = async (e) => {
-//   e.preventDefault()
-
-//   const data = new FormData(form)
-
-//   // user's chatstripe
-//   chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
-
-//   // to clear the textarea input 
-//   form.reset()
-
-//   // bot's chatstripe
-//   const uniqueId = generateUniqueId()
-//   chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
-
-//   // to focus scroll to the bottom 
-//   chatContainer.scrollTop = chatContainer.scrollHeight;
-
-//   // specific message div 
-//   const messageDiv = document.getElementById(uniqueId)
-
-//   // messageDiv.innerHTML = "..."
-//   loader(messageDiv)
-
-//   const response = await fetch('https://openai-codehub.onrender.com', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({
-//       prompt: data.get('prompt')
+};
 
 const handleSubmit = async (e) => {
-      e.preventDefault()
+  e.preventDefault();
 
-      const data = new FormData(form)
+  const data = new FormData(form);
 
-      // user's chatstripe
-      chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  //user's chatStripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  form.reset();
 
-      // to clear the textarea input 
-      form.reset()
+  //bot ChatStripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 
-      // bot's chatstripe
-      const uniqueId = generateUniqueId()
-      chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  const messageDiv = document.getElementById(uniqueId);
+  loader(messageDiv);
 
-      // to focus scroll to the bottom 
-      chatContainer.scrollTop = chatContainer.scrollHeight;
-
-      // specific message div 
-      const messageDiv = document.getElementById(uniqueId)
-
-      // messageDiv.innerHTML = "..."
-      loader(messageDiv)
-
-      const response = await fetch('https://openai-codehub.onrender.com', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                  prompt: data.get('prompt')
+  //fetch data from the server => bot response
+    const response = await fetch('https://openai-codehub.onrender.com', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt')
+      })
     })
-   })
 
-   clearInterval(loadInterval)
-   messageDiv.innerHTML = " ";
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = " ";
 
-  if (response.ok) {
-    const data = await response.json();
-    const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-
-    typeText(messageDiv, parsedData)
-  } else {
-    const err = await response.text()
-
-    messageDiv.innerHTML = "Something went wrong"
-    alert(err)
+    if (response.ok) {
+      const data = await response.json();
+      const parsedData = data.bot.trim(); // trims any trailing spaces/'\n' 
+      typeText(messageDiv, parsedData)
+    } else {
+      const err = await response.text();
+      messageDiv.innerHTML = "Something went wrong";
+      alert(err);
+    }
   }
-}
 
-form.addEventListener('submit', handleSubmit)
+
+form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) {
-    handleSubmit(e)
+    handleSubmit(e);
   }
-})
+});
